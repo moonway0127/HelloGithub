6793ee3f9f229ceb62bfe400c2a753f60bff2a8d
moonway <moonway0127@gmail.com>
2017-09-09 14:07:05 +0800

    intent

--- app/src/main/java/com/example/moonway/hellogithub/MainActivity.java
+++ app/src/main/java/com/example/moonway/hellogithub/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.moonway.hellogithub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView  = (TextView) findViewById(R.id.textview);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),HopeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
